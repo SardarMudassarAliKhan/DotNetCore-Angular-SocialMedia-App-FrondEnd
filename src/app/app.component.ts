29a3dbddbac8fc4df1611cf68f73a4e26797fd22
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2025-04-21 01:00:14 +0500

    feat: integrate Bootstrap and ngx-bootstrap for UI components
    - Added dependencies for Bootstrap, ngx-bootstrap, and ng-bootstrap in package.json.
    - Updated app.component.html to utilize Bootstrap classes and structure.
    - Implemented HTTP client to fetch user data from the API in app.component.ts.
    - Configured HttpClient in app.config.ts for dependency injection.
    - Added Angular localization support in main.ts and updated tsconfig files.
    - Created a new settings.json for VSCode to enhance development experience.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  http = inject(HttpClient);
   title = 'DotNetCore-Angular-SocialMedia-App';
+
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:44383/api/User').subscribe({
+      next: (response) => {
+        console.log(response);
+        this.users = response;
+      },
+      error: (error) => {
+        console.error('Error fetching data:', error);
+      },
+      complete: () => {
+        console.log('Request completed');
+      }
+    });
+  }
 }
